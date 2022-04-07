8a5b6a77964e6f042908d0b752218c1cf0605639
AfsanaAbsar <afsanaabsarchy@gmail.com>
2022-04-07 15:37:05 +0600

    error updated

--- js/app.js
+++ js/app.js
@@ -4,34 +4,54 @@ function getInput(inputParam) {
     const input = document.getElementById(inputParam);
     const inputAmountValue = input.value;
     const inputAmount = parseFloat(inputAmountValue);
+
     return inputAmount;
+
 }
 
 document.getElementById('calculate-btn').addEventListener('click', function () {
 
     // income
     const incomeAmount = getInput('income-input');
-    // errorhandle(incomeAmount);
+
 
     // food
     const foodAmount = getInput('food-input');
-    // errorhandle(foodAmount)
+
 
     // rent
     const rentAmount = getInput('rent-input');
-    // errorhandle(rentAmount)
+
 
     // clothes
     const clothesAmount = getInput('clothes-input');
-    // errorhandle(clothesAmount)
-
-    const totalExpenses = document.getElementById('total-expenses');
-    totalExpenses.innerText = foodAmount + rentAmount + clothesAmount;
-    const expenses = totalExpenses.innerText;
 
 
-    const totalBalance = document.getElementById('total-balance');
-    totalBalance.innerText = incomeAmount - parseFloat(expenses);
+    // input error handler
+
+    if (incomeAmount && foodAmount && rentAmount && clothesAmount) {
+        const totalExpenses = document.getElementById('total-expenses');
+        totalExpenses.innerText = foodAmount + rentAmount + clothesAmount;
+        const expenses = totalExpenses.innerText;
+        // balance error handler
+        if (expenses < incomeAmount) {
+            const totalBalance = document.getElementById('total-balance');
+            totalBalance.innerText = incomeAmount - parseFloat(expenses);
+
+            const balanceError = document.getElementById('balance');
+            balanceError.style.display = 'none';
+        }
+        else {
+            const balanceError = document.getElementById('balance');
+            balanceError.style.display = 'block';
+        }
+        const inputError = document.getElementById('error');
+        inputError.style.display = 'none';
+    }
+    else {
+        const inputError = document.getElementById('error');
+        inputError.style.display = 'block';
+    }
 
 
 });
@@ -40,28 +60,33 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
 // Handle Savings Button Events
 
 document.getElementById('saving-btn').addEventListener('click', function () {
-    const incomeAmount = document.getElementById('income-input');
-    const incomeValue = incomeAmount.innerText;
-    const income = parseFloat(incomeValue);
 
-    const balanceAmount = document.getElementById('total-balance');
-    const balanceValue = balanceAmount.innerText;
-    const balance = parseFloat(balanceValue);
-    console.log(balance);
+    const savingPercantageInput = document.getElementById('saving-input');
+    const savingPercantage = savingPercantageInput.value;
 
 
+    const incomeAmount = getInput('income-input');
+    const totalBalance = document.getElementById('total-balance');
+    const balance = parseFloat(totalBalance.innerText);
 
     // saving calculation
 
-    const savingAmountPercantage = getInput('saving-input');
     const savingAmount = document.getElementById('saving-amount');
-    savingAmount.innerText = savingAmountPercantage * income * 0.01;
-    const savings = savingAmount.innerText;
+    savingAmount.innerText = incomeAmount * parseFloat(savingPercantage) * 0.01;
+    const savings = parseFloat(savingAmount.innerText);
+
 
     // remaining balance update
 
-    const remainingAmount = document.getElementById('remaining-amount');
-    remainingAmount.innerText = balance - parseFloat(savings);
+    const savingError = document.getElementById('saving-error');
+    if (totalBalance.innerText > savingAmount.innerText) {
+        const remainingBalanceField = document.getElementById('remaining-amount');
+        remainingBalanceField.innerText = parseFloat(balance) - parseFloat(savings);
+        savingError.style.display = 'none';
 
+    }
+    else {
+        savingError.style.display = 'block';
+    }
 
 })
